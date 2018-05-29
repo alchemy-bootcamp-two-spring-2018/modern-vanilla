a561970a990ffc5496bf493f2f133a841b0b5a79
carriecarter <carrie@carrie-carter.com>
2018-05-29 16:30:56 -0700

    basic functionality in place

---
+++ js/App.js
@@ -0,0 +1,34 @@
+import Template from './Template.js';
+import IpsumList from './IpsumList.js';
+import IpsumViewer from './IpsumViewer.js';
+
+const template = new Template(() => {
+return `
+        <main>
+            <h1>Lorem Ipsum Varieties</h1>
+            <section id="list"></section>
+            <section id="viewer"></section>
+        </main>
+    `;    
+});
+
+export default class App {
+    render() {
+        const dom = template.render();
+
+        const ipsumList = new IpsumList(selected => {
+            ipsumViewer.update(selected);
+        });
+
+        const section = dom.getElementById('list');
+        const listDom = ipsumList.render();
+        section.appendChild(listDom);
+
+        const ipsumViewer = new IpsumViewer();
+        const viewer = dom.getElementById('viewer');
+        viewer.appendChild(ipsumViewer.render());
+
+        return dom;
+    }
+}
+
