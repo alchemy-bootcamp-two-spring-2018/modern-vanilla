da201e1a24454b1449b4bf94e961bc2862a8027f
Chris Golden <chrgolden@gmail.com>
2018-05-29 14:36:15 -0700

    data template rendering into the list

--- js/IpsumList.js
+++ js/IpsumList.js
@@ -1,25 +1,36 @@
 import Template from './Template.js';
 import data from './data.js';
 
-const template = new Template(({ length }) => {`
-    <h2>${length} Title></h2>
+const listTemplate = new Template(({ length }) => `
+    <h2>${length} Ipsums!</h2>
     <ul class="list">Unordered List</ul>
-    `;
-});
+    `
+);
+
+const dataTemplate = new Template(() => `
+    <li class="data"> 
+        li data template
+    </li>
+    
+`);
 
 export default class IpsumList {
     constructor(onSelect) {
         this.dataList = data;
-        this.onSelect = onSelect;
+    //     this.onSelect = onSelect;
     }
 
     render() {
-        const dom = template.render(this.dataList);
-        // const ul = dom.querySelector('ul');
-        // this.dataList.map(ipsums => {
-        //     this.onSelect(ipsums):
-        // });
-        
+        const dom = listTemplate.render(this.dataList);
+        const ul = dom.querySelector('ul');
+        this.dataList.map(ipsums => {
+            const dom = dataTemplate.render(data);
+            const li = dom.querySelector('li');
+            // this.onSelect(ipsums):
+            ul.appendChild(dom);
+
+        });
+
 
         return dom;
     }
